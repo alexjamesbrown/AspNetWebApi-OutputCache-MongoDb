876c2aa4d268e93ed589335db7b044f3ad06aa64
Alex Brown <alex.brown@ink-global.com>
2014-02-07 16:48:47 +0000

    added TTL index on collection test takes over 1 minute to run, because of the way TTL collections are cleared!

--- WebAPI.OutputCache.MongoDb.Tests/MongoDbApiOutputCacheTests.cs
+++ WebAPI.OutputCache.MongoDb.Tests/MongoDbApiOutputCacheTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Threading;
+using MongoDB.Driver.Builders;
+using NUnit.Framework;
 
 namespace WebAPI.OutputCache.MongoDb.Tests
 {
@@ -17,6 +20,24 @@ namespace WebAPI.OutputCache.MongoDb.Tests
         {
             var mongoDbApiOutputCache = new MongoDbApiOutputCache(MongoDatabase, "myCache");
             Assert.That(mongoDbApiOutputCache.MongoCollection.Name, Is.EqualTo("myCache"));
+        }
+
+        //due to the nature of the way mongodb ttl collections work this test takes over a minute to run
+        //we must sleep the thread for over a minute, as the process that removes expired documents runs once a minute or so!
+        [Test]
+        public void items_are_deleted_from_database_automatically_when_expired()
+        {
+            var mongoDbApiOutputCache = new MongoDbApiOutputCache(MongoDatabase);
+
+            mongoDbApiOutputCache.Add("something", "a value", DateTime.Now.AddMilliseconds(250));
+
+            //wait just over a minute....
+            Thread.Sleep(61000);
+
+            //get the thing directly from mongo
+            var resultDirectlyFromMongo = MongoCollection.FindOneAs<CachedItem>(Query.EQ("_id", "something"));
+
+            Assert.That(resultDirectlyFromMongo, Is.Null);
         }
     }
 }
