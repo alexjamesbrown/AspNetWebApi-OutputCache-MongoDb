a1d8c6e2ef762fd58afd4bab26659d06ed112749
Alex Brown <alex.brown@ink-global.com>
2014-02-10 18:33:18 +0000

    should update version number automatically

--- WebAPI.OutputCache.MongoDb/Properties/AssemblyInfo.cs
+++ WebAPI.OutputCache.MongoDb/Properties/AssemblyInfo.cs
@@ -7,12 +7,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("AspNetWebApi-OutputCache-MongoDb")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AspNetWebApi-OutputCache-MongoDb")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("WebAPI.OutputCache.MongoDb.Tests")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -31,6 +27,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
